2f8fc883730b0ac2e0819e78718b0623712389e3
Ross Harrison <rtharrison86@gmail.com>
2017-07-24 22:35:05 -0400

    Data reads and writes and guards and prints

--- priority-reader-writer/main.cpp
+++ priority-reader-writer/main.cpp
@@ -1,22 +1,53 @@
 #include <stdio.h>
 #include <pthread.h>
-#define NUM_THREADS 4
+#define NUM_THREADS 100
 
-pthread_mutex_t resource_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t counter_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t read_phase = PTHREAD_COND_INITIALIZER;
 pthread_cond_t write_phase = PTHREAD_COND_INITIALIZER;
+int resourceCounter = 0;
+int readerCount = 0;
+
+int dataToWrite = 0;
 
 void* readFunc(void *pArg) {
-    int* p = (int*) pArg;
-    int myNum = *p;
-    printf("Read Thread number %d\n", myNum);
+    pthread_mutex_lock(&counter_mutex);
+        while(resourceCounter == -1) {
+            pthread_cond_wait(&read_phase, &counter_mutex);
+        }
+        resourceCounter++;
+        readerCount++;
+    pthread_mutex_unlock(&counter_mutex);
+
+    printf("Reading data %d\n", dataToWrite);
+
+    pthread_mutex_lock(&counter_mutex);
+        resourceCounter--;
+        readerCount--;
+        if(resourceCounter == 0) {
+            pthread_cond_signal(&write_phase);
+        }
+    pthread_mutex_unlock(&counter_mutex);
    return 0;
 }
 
 void* writeFunc(void *pArg) {
-    int* p = (int*) pArg;
-    int myNum = *p;
-    printf("Write Thread number %d\n", myNum);
+    pthread_mutex_lock(&counter_mutex);
+    while(resourceCounter != 0) {
+        pthread_cond_wait(&write_phase, &counter_mutex);
+    }
+    resourceCounter = -1;
+    pthread_mutex_unlock(&counter_mutex);
+
+    dataToWrite++;
+    printf("Writing data %d; Reader Count: %d\n", dataToWrite, readerCount);
+
+    pthread_mutex_lock(&counter_mutex);
+        resourceCounter = 0;
+        pthread_cond_broadcast(&read_phase);
+        pthread_cond_signal(&write_phase);
+    pthread_mutex_unlock(&counter_mutex);
+
     return 0;
 }
 
@@ -34,7 +65,7 @@ int main() {
 
     for (int i = 0; i < NUM_THREADS; i++) {
         writerVals[i] = i + 100;
-        pthread_create(&writers[i], NULL, readFunc, &writerVals[i]);
+        pthread_create(&writers[i], NULL, writeFunc, &writerVals[i]);
     }
 
     for (int i = 0; i < NUM_THREADS; i++) {
